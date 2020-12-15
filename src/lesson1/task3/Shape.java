283aa25b373fe846c85e89eb0b49628a36f206c5
Valery Bulekov <muzaccord@gmail.com>
2020-12-15 19:57:55 +0300

    Lesson 1 homework

---
+++ src/lesson1/task3/Shape.java
@@ -0,0 +1,13 @@
+package lesson1.task3;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    abstract double getArea();
+}
