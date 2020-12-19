47ae88c9e8fd129f94d6c631ca52ab1621a1f3da
Valery Bulekov <muzaccord@gmail.com>
2020-12-19 17:44:02 +0300

    Lesson 2. LinkedList

--- src/lesson2/LinkedListDemo.java
+++ src/lesson2/LinkedListDemo.java
@@ -1,38 +1,141 @@
 package lesson2;
 
 public class LinkedListDemo<E> implements ListDemo<E>{
+
+    class Node<V> {
+        V value;
+        Node<V> next;
+        Node<V> previous;
+
+        public Node(V value, Node<V> next, Node<V> previous) {
+            this.value = value;
+            this.next = next;
+            this.previous = previous;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public Node<V> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<V> next) {
+            this.next = next;
+        }
+
+        public Node<V> getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node<V> previous) {
+            this.previous = previous;
+        }
+    }
+
+    private int size;
+    private Node<E> first;
+    private Node<E> last;
+
+    public LinkedListDemo() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size==0;
     }
 
     @Override
-    public boolean contains(E o) {
-        return false;
+    public boolean contains(E entry) {
+        Node<E> node = searchNode(entry);
+        return node!=null;
+    }
+
+    private Node<E> searchNode(E entry) {
+        if (size==0) return null;
+        Node<E> current = first;
+        while (current!=null){
+            if (current.getValue().equals(entry)){
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
     }
 
     @Override
-    public boolean add(E o) {
-        return false;
+    public boolean add(E entry) {
+        if (last==null){
+            last = new Node<>(entry, null, null);
+            first = last;
+        } else {
+            Node<E> tmp = last;
+            last = new Node<>(entry, null, tmp);
+            tmp.setNext(last);
+        }
+
+        size++;
+        return true;
     }
 
     @Override
-    public boolean remove(E o) {
-        return false;
+    public boolean remove(E entry) {
+        Node<E> node = searchNode(entry);
+
+        if (node == null) return false;
+
+        if (!(node.equals(first)||node.equals(last))){
+            node.getPrevious().setNext(node.getNext());
+            node.getNext().setPrevious(node.getPrevious());
+        }
+
+        if (node.equals(first)){
+            first = node.getNext();
+        }
+
+        if (node.equals(last)){
+            last = node.getPrevious();
+        }
+
+        size--;
+        return true;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        Node<E> node = getNodeByIndex(index);
+        if (node == null) return null;
+        return node.getValue();
+    }
+
+    private Node<E> getNodeByIndex(int index) {
+        if (index >=size) return null;
+
+        Node<E> current = first;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current;
     }
 
     @Override
-    public E set(int index, E element) {
-        return null;
+    public E set(int index, E entry) {
+        Node<E> node = getNodeByIndex(index);
+        if (node == null) return null;
+        node.setValue(entry);
+        return entry;
     }
 }
--- src/lesson2/MainApp.java
+++ src/lesson2/MainApp.java
@@ -2,34 +2,36 @@ package lesson2;
 
 public class MainApp {
 
-    private static ListDemo<String> arrayList;
+    private static ListDemo<String> list;
 
     public static void main(String[] args) {
         System.out.println("=============ArrayList=============");
-        arrayList = new ArrayListDemo<>();
+        list = new ArrayListDemo<>();
         arrayDemo();
 
         System.out.println("\n=============LinkedList=============");
-        arrayList = new LinkedListDemo<>();
+        list = new LinkedListDemo<>();
         arrayDemo();
     }
 
     private static void arrayDemo() {
-        System.out.println("Is list empty: " + arrayList.isEmpty());
+        System.out.println("Is list empty: " + list.isEmpty());
 
         for (int i = 0; i < 25; i++) {
-            arrayList.add("Item " + i);
+            list.add("Item " + i);
         }
 
-        System.out.println("List size after adding 25 entities: " + arrayList.size());
-        System.out.println("Getting entity by index 15: " + arrayList.get(15));
+        System.out.println("List size after adding 25 entities: " + list.size());
+        System.out.println("Getting entity by index 15: " + list.get(15));
         System.out.println("Removing entity by index 15.");
-        arrayList.remove("Item 15");
-        System.out.println("Getting entity by index 15: " + arrayList.get(15));
-        System.out.println("List size: " + arrayList.size());
-        System.out.println("Getting entity by index 24: " + arrayList.get(24));
-        System.out.println("Setting entity by index 15: " + arrayList.set(15, "New Item"));
-        System.out.println("Getting entity by index 15: " + arrayList.get(15));
-        System.out.println("Setting entity by index 24 (more then list size): " + arrayList.set(24, "Wrong index"));
+        list.remove("Item 15");
+        System.out.println("Getting entity by index 15: " + list.get(15));
+        System.out.println("List size: " + list.size());
+        System.out.println("Getting entity by index 24: " + list.get(24));
+        System.out.println("Setting entity by index 15: " + list.set(15, "New Item"));
+        System.out.println("Getting entity by index 15: " + list.get(15));
+        System.out.println("Setting entity by index 24 (more then list size): " + list.set(24, "Wrong index"));
+        System.out.println("Does list contain value 'Item 20': " + list.contains("Item 20"));
+        System.out.println("Does list contain value 'Item 15': " + list.contains("Item 15"));
     }
 }
