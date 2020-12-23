3c4b4d8c02de2fb089362fdba9b2fad9b026a659
Valery Bulekov <muzaccord@gmail.com>
2020-12-23 18:24:52 +0300

    Lesson 3. Ping pong

---
+++ src/lesson3/PingPongApp.java
@@ -0,0 +1,20 @@
+package lesson3;
+
+public class PingPongApp {
+
+    public static void main(String[] args) {
+        PingPongClass pp = new PingPongClass();
+        Thread t1 = new Thread(()->{
+        while (true) {
+                pp.ping();
+            }
+        });
+        Thread t2 = new Thread(()->{
+            while (true) {
+                pp.pong();
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
